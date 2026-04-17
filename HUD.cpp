5b3db042f3d5902391561c9f00b4d4d0ba1ec5b5
a24irifragra@inspedralbes.cat <a24irifragra@inspedralbes.cat>
2026-04-17 16:14:08 +0200

    HUD

--- HUD.cpp
+++ HUD.cpp
@@ -18,8 +18,25 @@ void HUD::Update()
 
 void HUD::Render()
 {
-	GI->DrawText("Score: ", 100, 600);
 
+	GI->DrawText("Score: ", 100, 600);
+	GI->DrawText("0 ", 220, 600);
+
+	Transform t;
+	t.position = { 300, 600 };   // posición en pantalla
+	t.rotation = 0;            // sin rotación
+	t.scale = { 1, 1 };          // escala normal
+
+	GI->DrawSprite("Assets\Imagenes\GranadaIcon.png", t, 20, 20);
+	GI->DrawText("0 ", 450, 600);
+
+	GI->DrawText("MEN: ", 500, 600);
+	GI->DrawText("0 ", 600, 600);
+
+	GI->DrawText("HI: ", 700, 600);
+	GI->DrawText("0 ", 800, 600);
+
+	
 }
 
 void HUD::HandleInput()
